9034050cdb8c3cfb94ac4b9906a23290cb86de8b
khansa-nadhifa <59870203+khansa-nadhifa@users.noreply.github.com>
2020-01-17 11:03:05 +0700

    first commit

---
+++ konten/galeri.php
@@ -0,0 +1,30 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>mywebsite</title>
+		<link rel="stylesheet" type="text/css" href="../style/style.css"/>
+</head>
+<body>
+	<div id="container">
+		<div id="header">
+			<h1>pertemuan 1 -- SISTEM PAKAR</h1>
+		</div>
+		<div id="sidebar">
+			<h3>navigasi</h3>
+			<ul id="navmenu">
+				<li><a href="../index.html">profil</a></li>
+				<li><a href="galeri.php" class="selected">galeri</a></li>
+				<li><a href="jadwal.php">jadwal kuliah</a></li>
+			</ul>
+		</div>
+		<div id="page">   
+			<p><img src="../images/little_einstein.jpg" width="200" height="150"></p>
+			<p><img src="../images/tayo.jpg" width="250" height="150"></p>	
+			<p><img src="../images/doraemon.png" width="145" height="117"> </p>		
+		</div>
+		<div id="clear"></div>
+		<div id="footer">
+			<p>&copy; 2019</p>
+		</div>
+	</div>
+</body>
+</html>
---
+++ konten/jadwal.php
@@ -0,0 +1,95 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>mywebsite</title>
+		<link rel="stylesheet" type="text/css" href="../style/style.css"/>
+</head>
+<body>
+	<div id="container">
+		<div id="header">
+			<h1>pertemuan 1 -- SISTEM PAKAR</h1>
+		</div>
+		<div id="sidebar">
+			<h3>navigasi</h3>
+			<ul id="navmenu">
+				<li><a href="../index.html">profil</a></li>
+				<li><a href="galeri.php">galeri</a></li>
+				<li><a href="jadwal.php" class="selected">jadwal kuliah</a></li>
+			</ul>
+		</div>
+		<div id="page">
+			<p> Jadwal Perkuliahan </p>
+			<table width="500" border="1">
+				<tr>
+					<td width="22" bgcolor="#7FFFD4">No</td>
+					<td width="200" bgcolor="#7FFFD4">Mata Kuliah</td>
+					<td width="65" bgcolor="#7FFFD4">Hari</td>
+					<td width="120" bgcolor="#7FFFD4">Waktu</td>
+				</tr>
+				
+				<tr>
+					<td>1</td>
+					<td>Matematika Dasar</td>
+					<td>Senin</td>
+					<td>11.00-12.45</td>
+				</tr>
+				
+				<tr>
+					<td>2</td>
+					<td>Matematika Numerik</td>
+					<td>Senin</td>
+					<td>13.30-16.00</td>
+				</tr>
+				
+				<tr>
+					<td>3</td>
+					<td>Organisasi Komputer</td>
+					<td>Selasa</td>
+					<td>13.00-15.30</td>
+				</tr>
+				
+				
+				<tr>
+					<td>4</td>
+					<td>Analisis dan Desain Sistem</td>
+					<td>Selasa</td>
+					<td>15.30-17.00</td>
+				</tr>
+				
+				<tr>
+					<td>5</td>
+					<td>Web Programming</td>
+					<td>Kamis</td>
+					<td>07.30-10.00</td>
+				</tr>
+				
+				<tr>
+					<td>6</td>
+					<td>Komunikasi Data</td>
+					<td>Kamis</td>
+					<td>10.00-12.00</td>
+				</tr>
+				
+				<tr>
+					<td>7</td>
+					<td>Rancangan Perangkat Lunak</td>
+					<td>Kamis</td>
+					<td>13.00-16.00</td>
+				</tr>
+				
+				<tr>
+					<td>8</td>
+					<td>keamanan Teknologi Informasi</td>
+					<td>Jumat</td>
+					<td>13.30-16.00</td>
+				</tr>
+			</table>
+
+
+		</div>
+		<div id="clear"></div>
+		<div id="footer">
+			<p>&copy; 2019</p>
+		</div>
+	</div>
+</body>
+</html>
